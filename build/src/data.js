e1831a061c72e40823fd1d7fd11eb7fd7757318b
MajjigaPavani <pavanima05@gmail.com>
2026-03-08 21:55:30 +0530

    Add files via upload

---
+++ build/src/data.js
@@ -0,0 +1,7 @@
+// data.js
+const initialStudents = [
+  { id: 1, name: "Pa Mmmmmm", email: "pa@example.com", age: 22, percentage: 78 },
+  { id: 2, name: "John Doe", email: "john@example.com", age: 23, percentage: 85 },
+];
+
+export default initialStudents;
